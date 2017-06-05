13d68c2c4308707cd5e27effdcc1da73fea8bf2d
Luigy Leon <luigy@outlook.com>
2017-06-05 13:45:20 -0400

    🌅 Add FromJSON and ToJSON instances for SelectedCount and AppendMap.

---
+++ src/Reflex/Orphans.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+module Reflex.Orphans () where
+
+import Data.Aeson
+import Data.AppendMap
+import qualified Data.Map as Map
+import Reflex
+
+instance (ToJSON k, ToJSON m) => ToJSON (AppendMap k m) where
+  toJSON = toJSON . Map.toList . _unAppendMap
+
+instance (FromJSON k, FromJSON m, Ord k) => FromJSON (AppendMap k m) where
+  parseJSON r = do
+    res <- parseJSON r
+    fmap AppendMap . sequence . Map.fromListWithKey (fail "duplicate key in JSON deserialization of AppendMap") . fmap (fmap return) $ res
+
+deriving instance FromJSON SelectedCount
+deriving instance ToJSON SelectedCount
