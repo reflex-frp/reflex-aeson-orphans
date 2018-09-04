324904d80ad7c4809f64797dd26d987b50d74d16
Cale Gibbard <cgibbard@gmail.com>
2018-09-03 21:03:36 -0400

    Remove JSON instances for MonoidalMap, they are now part of the monoidal-containers package.

--- src/Reflex/Aeson/Orphans.hs
+++ src/Reflex/Aeson/Orphans.hs
@@ -7,13 +7,5 @@ import Data.Map.Monoidal (MonoidalMap (..))
 import qualified Data.Map as Map
 import Reflex
 
-instance (ToJSON k, ToJSON m) => ToJSON (MonoidalMap k m) where
-  toJSON = toJSON . Map.toList . getMonoidalMap
-
-instance (FromJSON k, FromJSON m, Ord k) => FromJSON (MonoidalMap k m) where
-  parseJSON r = do
-    res <- parseJSON r
-    fmap MonoidalMap . sequence . Map.fromListWithKey (fail "duplicate key in JSON deserialization of AppendMap") . fmap (fmap return) $ res
-
 deriving instance FromJSON SelectedCount
 deriving instance ToJSON SelectedCount
